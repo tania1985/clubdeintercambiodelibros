c0f52a818add1d249a0f78623268deb620dda2c2
tania1985 <testevezbautista@danielcastelao.org>
2025-03-20 19:45:19 +0100

    modelos distintos

--- prestamos/admin.py
+++ prestamos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Prestamo
+from .models import Estado
+
+admin.site.register(Prestamo)
+admin.site.register(Estado)
 # Register your models here.
