64828cdc250241565ff7619d3d6fab76bd842c8b
tania1985 <testevezbautista@danielcastelao.org>
2025-03-20 17:22:04 +0100

    imagenes

--- libros/urls.py
+++ libros/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'libros'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("alta/", views.alta_libro, name="alta_libro"),
 ]
--- libros/views.py
+++ libros/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .models import Libro
+from .forms import LibroForm
 
 
 def index(request):
     if(request.session.get('usuario_id')): 
-        nombre=request.session.get('nombre')
-        return render(request, 'index.html', {'nombre': nombre}) 
+        libros = Libro.objects.filter(usuario_id=request.session.get('usuario_id'))
+        return render(request, 'index.html', {'libros': libros}) 
     else:
         #redirijo a la pagina de login
         return redirect('usuarios:login')
+
+def alta_libro(request):
+    if request.method == 'POST':
+        form = LibroForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.usuario_id = request.session.get('usuario_id')
+            form.save()
+            return redirect('libros:index')  # Redirige a la lista de libros, por ejemplo.
+    else:
+        form = LibroForm()
+    return render(request, 'alta_libro.html', {'form': form})
